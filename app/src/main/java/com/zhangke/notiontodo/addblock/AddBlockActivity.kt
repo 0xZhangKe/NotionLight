7ff83661de4efe88237db1a8e2ad3b859f6dbd21
ZhangKe <kezhang404@gmail.com>
2022-04-23 23:34:06 +0800

    working in AddBlock page ui

--- app/src/main/java/com/zhangke/notiontodo/addblock/AddBlockActivity.kt
+++ app/src/main/java/com/zhangke/notiontodo/addblock/AddBlockActivity.kt
@@ -5,17 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.SelectAll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,6 +33,10 @@ import com.zhangke.notiontodo.R
 
 class AddBlockActivity : ComponentActivity() {
 
+    companion object {
+
+        const val INTENT_ARG_PAGE = "arg_page"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,29 +58,85 @@ class AddBlockActivity : ComponentActivity() {
                 .background(Color(0x99000000)),
         ) {
             Surface(
-                shape = RoundedCornerShape(20),
                 modifier = Modifier
-                    .padding(20.dp, statusBarHeight + 20.dp, 20.dp, 20.dp)
-                    .background(Color.White)
-                    .weight(2F)
+                    .fillMaxWidth()
+                    .padding(20.dp, statusBarHeight + 20.dp, 20.dp, 0.dp)
+                    .weight(2F),
+                shape = RoundedCornerShape(18.dp),
             ) {
                 Column(
-                    verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Text(
-                        modifier = Modifier.padding(top = 20.dp),
+                        modifier = Modifier.padding(top = 15.dp),
                         text = getString(R.string.add_block_title),
                         fontSize = 18.sp,
                         color = Color.Black,
                     )
 
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp, 15.dp, 20.dp, 0.dp)
+                    ) {
+                        Text(text = getString(R.string.add_block_item_page_type))
+                        Spacer(modifier = Modifier.weight(1F))
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.clickable {
+
+                            }
+                        ) {
+                            val pageConfig = vm.currentPage.observeAsState()
+                            pageConfig.value?.title?.let {
+                                Text(text = it)
+                            }
+                            Icon(
+                                modifier = Modifier.padding(start = 3.dp),
+                                painter = rememberVectorPainter(image = Icons.Filled.ArrowDropDown),
+                                contentDescription = "Select other"
+                            )
+                        }
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp, 15.dp, 20.dp, 0.dp)
+                    ) {
+                        Text(text = getString(R.string.add_block_item_block_type))
+                        Spacer(modifier = Modifier.weight(1F))
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.clickable {
+
+                            }
+                        ) {
+                            val blockType = vm.currentBlockType.observeAsState()
+                            blockType.value?.let {
+                                Text(text = it)
+                            }
+                            Icon(
+                                modifier = Modifier.padding(start = 3.dp),
+                                painter = rememberVectorPainter(image = Icons.Filled.ArrowDropDown),
+                                contentDescription = "Select other"
+                            )
+                        }
+                    }
+
                     TextField(
                         value = inputtedText,
+                        shape = RoundedCornerShape(6.dp),
                         onValueChange = { inputtedText = it },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color(0x99CCCCCC),
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ),
                         modifier = Modifier
+                            .fillMaxWidth()
                             .padding(20.dp, 20.dp, 20.dp, 20.dp)
-                            .fillMaxWidth(),
+                            .defaultMinSize(minHeight = 80.dp),
                     )
                 }
             }
@@ -75,20 +144,19 @@ class AddBlockActivity : ComponentActivity() {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(20.dp, 20.dp, 20.dp, 0.dp)
+                    .padding(20.dp, 10.dp, 20.dp, 0.dp)
             ) {
                 Button(
                     shape = RoundedCornerShape(30.dp),
-                    modifier = Modifier
-                        .height(30.dp)
-                        .padding(start = 10.dp, end = 10.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White),
                     onClick = {
-
+                        finish()
                     }
                 ) {
                     Text(
                         color = Color.Black,
                         textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(start = 10.dp, end = 10.dp),
                         text = getString(R.string.cancel)
                     )
                 }
@@ -97,9 +165,7 @@ class AddBlockActivity : ComponentActivity() {
 
                 Button(
                     shape = RoundedCornerShape(30.dp),
-                    modifier = Modifier
-                        .height(30.dp)
-                        .padding(start = 10.dp, end = 10.dp),
+                    colors = ButtonDefaults.buttonColors(Color.White),
                     onClick = {
 
                     }
@@ -107,6 +173,7 @@ class AddBlockActivity : ComponentActivity() {
                     Text(
                         color = Color.Black,
                         textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(start = 10.dp, end = 10.dp),
                         text = getString(R.string.ok)
                     )
                 }
--- app/src/main/java/com/zhangke/notiontodo/addblock/AddBlockViewModel.kt
+++ app/src/main/java/com/zhangke/notiontodo/addblock/AddBlockViewModel.kt
@@ -1,8 +1,54 @@
 package com.zhangke.notiontodo.addblock
 
+import android.content.Intent
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zhangke.notiontodo.config.NotionPageConfig
+import com.zhangke.notiontodo.config.NotionPageConfigRepo
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class AddBlockViewModel: ViewModel() {
+class AddBlockViewModel : ViewModel() {
 
+    val currentPage = MutableLiveData<NotionPageConfig?>()
+    val currentBlockType = MutableLiveData<String?>()
 
+    private var targetPageName: String? = null
+        set(value) {
+            field = value
+            findTargetPage()
+        }
+
+    private var pageList: List<NotionPageConfig>? = null
+        set(value) {
+            field = value
+            findTargetPage()
+        }
+
+    init {
+        viewModelScope.launch {
+            NotionPageConfigRepo.getPageConfigList()
+                .catch { }
+                .collect { pageList = it }
+        }
+    }
+
+    fun parseIntent(intent: Intent) {
+        targetPageName = intent.getStringExtra(AddBlockActivity.INTENT_ARG_PAGE)
+    }
+
+    private fun findTargetPage() {
+        val pageList = pageList ?: return
+        currentPage.value =
+            pageList.firstOrNull {
+                if (targetPageName.isNullOrEmpty()) {
+                    true
+                } else {
+                    it.title == targetPageName
+                }
+            }
+        currentBlockType.value = currentPage.value?.type?.value
+    }
 }
